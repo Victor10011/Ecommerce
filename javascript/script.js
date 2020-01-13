839315e97a3364d568dff80ee7c141ec616e7834
Nwosunneji Victor <victornneji10@gmail.om>
2020-01-13 13:16:46 +0100

    Please you can start working on the files now

---
+++ javascript/script.js
@@ -0,0 +1,73 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
+
+
+//////////////AUTO SLIDES///////////////////
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 4000); 
+}
+
+/////////////////////////////////////////////////////////////////////
+$(document).ready(function(){
+		$('#main-toggle').on('click', function(){
+			$('.side-bar').toggle();
+		});
+
+		$('#home').on('click', function(){
+			$('.side-bar').hide();
+		});
+	});
+
+	$(document).ready(function(){
+		$('#mobile-category').on('click', function(){
+			$(select).toggle();
+		});
+
+	});
+
+///////////////////////////////////////
+
+var rangeslider = document.getElementById("sliderRange"); 
+var output = document.getElementById("demo"); 
+output.innerHTML = rangeslider.value; 
+  
+rangeslider.oninput = function() { 
+  output.innerHTML = this.value; 
+}
---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+		$('#main-toggle').on('click', function(){
+			$('.side-bar').toggle();
+		});
+
+		$('#home').on('click', function(){
+			$('.side-bar').hide();
+		});
+	});
+
+	$(document).ready(function(){
+		$('#mobile-category').on('click', function(){
+			$(select).toggle();
+		});
+
+	});
